341368b46985cb6e920d05ce3ca1a9880807c02e
Thomas Paine <tpaine@kineticsupply.com>
2014-10-17 11:12:10 -0500

    updated db credentials

--- wp-config.php
+++ wp-config.php
@@ -17,13 +17,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'test-wp');
+define('DB_NAME', 'wp_test');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root2');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
